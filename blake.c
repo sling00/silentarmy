653eca25584d03c6c5a5b21f3aa5e33e02f5b2d8
Alex <alex@gpushack.com>
2016-11-10 22:27:31 -0600

    unroll some more stuff in blake

--- blake.c
+++ blake.c
@@ -84,18 +84,127 @@ void zcash_blake2b_update(blake2b_state_t *st, const uint8_t *_msg,
     memcpy(v + 8, blake2b_iv, 8 * sizeof (*v));
     v[12] ^= (st->bytes += msg_len);
     v[14] ^= is_final ? -1 : 0;
-    for (uint32_t round = 0; round < blake2b_rounds; round++)
-      {
-        const uint8_t   *s = blake2b_sigma[round];
-        mix(v + 0, v + 4, v + 8,  v + 12, m[s[0]],  m[s[1]]);
-        mix(v + 1, v + 5, v + 9,  v + 13, m[s[2]],  m[s[3]]);
-        mix(v + 2, v + 6, v + 10, v + 14, m[s[4]],  m[s[5]]);
-        mix(v + 3, v + 7, v + 11, v + 15, m[s[6]],  m[s[7]]);
-        mix(v + 0, v + 5, v + 10, v + 15, m[s[8]],  m[s[9]]);
-        mix(v + 1, v + 6, v + 11, v + 12, m[s[10]], m[s[11]]);
-        mix(v + 2, v + 7, v + 8,  v + 13, m[s[12]], m[s[13]]);
-        mix(v + 3, v + 4, v + 9,  v + 14, m[s[14]], m[s[15]]);
-      }
+
+const uint8_t   *s0 = blake2b_sigma[0];
+        mix(v + 0, v + 4, v + 8,  v + 12, m[s0[0]],  m[s0[1]]);
+        mix(v + 1, v + 5, v + 9,  v + 13, m[s0[2]],  m[s0[3]]);
+        mix(v + 2, v + 6, v + 10, v + 14, m[s0[4]],  m[s0[5]]);
+        mix(v + 3, v + 7, v + 11, v + 15, m[s0[6]],  m[s0[7]]);
+        mix(v + 0, v + 5, v + 10, v + 15, m[s0[8]],  m[s0[9]]);
+        mix(v + 1, v + 6, v + 11, v + 12, m[s0[10]], m[s0[11]]);
+        mix(v + 2, v + 7, v + 8,  v + 13, m[s0[12]], m[s0[13]]);
+        mix(v + 3, v + 4, v + 9,  v + 14, m[s0[14]], m[s0[15]]);
+
+const uint8_t   *s1 = blake2b_sigma[1];
+        mix(v + 0, v + 4, v + 8,  v + 12, m[s1[0]],  m[s1[1]]);
+        mix(v + 1, v + 5, v + 9,  v + 13, m[s1[2]],  m[s1[3]]);
+        mix(v + 2, v + 6, v + 10, v + 14, m[s1[4]],  m[s1[5]]);
+        mix(v + 3, v + 7, v + 11, v + 15, m[s1[6]],  m[s1[7]]);
+        mix(v + 0, v + 5, v + 10, v + 15, m[s1[8]],  m[s1[9]]);
+        mix(v + 1, v + 6, v + 11, v + 12, m[s1[10]], m[s1[11]]);
+        mix(v + 2, v + 7, v + 8,  v + 13, m[s1[12]], m[s1[13]]);
+        mix(v + 3, v + 4, v + 9,  v + 14, m[s1[14]], m[s1[15]]);
+
+const uint8_t   *s2 = blake2b_sigma[2];
+        mix(v + 0, v + 4, v + 8,  v + 12, m[s2[0]],  m[s2[1]]);
+        mix(v + 1, v + 5, v + 9,  v + 13, m[s2[2]],  m[s2[3]]);
+        mix(v + 2, v + 6, v + 10, v + 14, m[s2[4]],  m[s2[5]]);
+        mix(v + 3, v + 7, v + 11, v + 15, m[s2[6]],  m[s2[7]]);
+        mix(v + 0, v + 5, v + 10, v + 15, m[s2[8]],  m[s2[9]]);
+        mix(v + 1, v + 6, v + 11, v + 12, m[s2[10]], m[s2[11]]);
+        mix(v + 2, v + 7, v + 8,  v + 13, m[s2[12]], m[s2[13]]);
+        mix(v + 3, v + 4, v + 9,  v + 14, m[s2[14]], m[s2[15]]);
+
+const uint8_t   *s3 = blake2b_sigma[3];
+        mix(v + 0, v + 4, v + 8,  v + 12, m[s3[0]],  m[s3[1]]);
+        mix(v + 1, v + 5, v + 9,  v + 13, m[s3[2]],  m[s3[3]]);
+        mix(v + 2, v + 6, v + 10, v + 14, m[s3[4]],  m[s3[5]]);
+        mix(v + 3, v + 7, v + 11, v + 15, m[s3[6]],  m[s3[7]]);
+        mix(v + 0, v + 5, v + 10, v + 15, m[s3[8]],  m[s3[9]]);
+        mix(v + 1, v + 6, v + 11, v + 12, m[s3[10]], m[s3[11]]);
+        mix(v + 2, v + 7, v + 8,  v + 13, m[s3[12]], m[s3[13]]);
+        mix(v + 3, v + 4, v + 9,  v + 14, m[s3[14]], m[s3[15]]);
+
+const uint8_t   *s4 = blake2b_sigma[4];
+        mix(v + 0, v + 4, v + 8,  v + 12, m[s4[0]],  m[s4[1]]);
+        mix(v + 1, v + 5, v + 9,  v + 13, m[s4[2]],  m[s4[3]]);
+        mix(v + 2, v + 6, v + 10, v + 14, m[s4[4]],  m[s4[5]]);
+        mix(v + 3, v + 7, v + 11, v + 15, m[s4[6]],  m[s4[7]]);
+        mix(v + 0, v + 5, v + 10, v + 15, m[s4[8]],  m[s4[9]]);
+        mix(v + 1, v + 6, v + 11, v + 12, m[s4[10]], m[s4[11]]);
+        mix(v + 2, v + 7, v + 8,  v + 13, m[s4[12]], m[s4[13]]);
+        mix(v + 3, v + 4, v + 9,  v + 14, m[s4[14]], m[s4[15]]);
+
+const uint8_t   *s5 = blake2b_sigma[5];
+        mix(v + 0, v + 4, v + 8,  v + 12, m[s5[0]],  m[s5[1]]);
+        mix(v + 1, v + 5, v + 9,  v + 13, m[s5[2]],  m[s5[3]]);
+        mix(v + 2, v + 6, v + 10, v + 14, m[s5[4]],  m[s5[5]]);
+        mix(v + 3, v + 7, v + 11, v + 15, m[s5[6]],  m[s5[7]]);
+        mix(v + 0, v + 5, v + 10, v + 15, m[s5[8]],  m[s5[9]]);
+        mix(v + 1, v + 6, v + 11, v + 12, m[s5[10]], m[s5[11]]);
+        mix(v + 2, v + 7, v + 8,  v + 13, m[s5[12]], m[s5[13]]);
+        mix(v + 3, v + 4, v + 9,  v + 14, m[s5[14]], m[s5[15]]);
+
+const uint8_t   *s6 = blake2b_sigma[6];
+        mix(v + 0, v + 4, v + 8,  v + 12, m[s6[0]],  m[s6[1]]);
+        mix(v + 1, v + 5, v + 9,  v + 13, m[s6[2]],  m[s6[3]]);
+        mix(v + 2, v + 6, v + 10, v + 14, m[s6[4]],  m[s6[5]]);
+        mix(v + 3, v + 7, v + 11, v + 15, m[s6[6]],  m[s6[7]]);
+        mix(v + 0, v + 5, v + 10, v + 15, m[s6[8]],  m[s6[9]]);
+        mix(v + 1, v + 6, v + 11, v + 12, m[s6[10]], m[s6[11]]);
+        mix(v + 2, v + 7, v + 8,  v + 13, m[s6[12]], m[s6[13]]);
+        mix(v + 3, v + 4, v + 9,  v + 14, m[s6[14]], m[s6[15]]);
+
+const uint8_t   *s7 = blake2b_sigma[7];
+        mix(v + 0, v + 4, v + 8,  v + 12, m[s7[0]],  m[s7[1]]);
+        mix(v + 1, v + 5, v + 9,  v + 13, m[s7[2]],  m[s7[3]]);
+        mix(v + 2, v + 6, v + 10, v + 14, m[s7[4]],  m[s7[5]]);
+        mix(v + 3, v + 7, v + 11, v + 15, m[s7[6]],  m[s7[7]]);
+        mix(v + 0, v + 5, v + 10, v + 15, m[s7[8]],  m[s7[9]]);
+        mix(v + 1, v + 6, v + 11, v + 12, m[s7[10]], m[s7[11]]);
+        mix(v + 2, v + 7, v + 8,  v + 13, m[s7[12]], m[s7[13]]);
+        mix(v + 3, v + 4, v + 9,  v + 14, m[s7[14]], m[s7[15]]);
+
+const uint8_t   *s8 = blake2b_sigma[8];
+        mix(v + 0, v + 4, v + 8,  v + 12, m[s8[0]],  m[s8[1]]);
+        mix(v + 1, v + 5, v + 9,  v + 13, m[s8[2]],  m[s8[3]]);
+        mix(v + 2, v + 6, v + 10, v + 14, m[s8[4]],  m[s8[5]]);
+        mix(v + 3, v + 7, v + 11, v + 15, m[s8[6]],  m[s8[7]]);
+        mix(v + 0, v + 5, v + 10, v + 15, m[s8[8]],  m[s8[9]]);
+        mix(v + 1, v + 6, v + 11, v + 12, m[s8[10]], m[s8[11]]);
+        mix(v + 2, v + 7, v + 8,  v + 13, m[s8[12]], m[s8[13]]);
+        mix(v + 3, v + 4, v + 9,  v + 14, m[s8[14]], m[s8[15]]);
+
+const uint8_t   *s9 = blake2b_sigma[9];
+        mix(v + 0, v + 4, v + 8,  v + 12, m[s9[0]],  m[s9[1]]);
+        mix(v + 1, v + 5, v + 9,  v + 13, m[s9[2]],  m[s9[3]]);
+        mix(v + 2, v + 6, v + 10, v + 14, m[s9[4]],  m[s9[5]]);
+        mix(v + 3, v + 7, v + 11, v + 15, m[s9[6]],  m[s9[7]]);
+        mix(v + 0, v + 5, v + 10, v + 15, m[s9[8]],  m[s9[9]]);
+        mix(v + 1, v + 6, v + 11, v + 12, m[s9[10]], m[s9[11]]);
+        mix(v + 2, v + 7, v + 8,  v + 13, m[s9[12]], m[s9[13]]);
+        mix(v + 3, v + 4, v + 9,  v + 14, m[s9[14]], m[s9[15]]);
+
+const uint8_t   *s10 = blake2b_sigma[10];
+        mix(v + 0, v + 4, v + 8,  v + 12, m[s10[0]],  m[s10[1]]);
+        mix(v + 1, v + 5, v + 9,  v + 13, m[s10[2]],  m[s10[3]]);
+        mix(v + 2, v + 6, v + 10, v + 14, m[s10[4]],  m[s10[5]]);
+        mix(v + 3, v + 7, v + 11, v + 15, m[s10[6]],  m[s10[7]]);
+        mix(v + 0, v + 5, v + 10, v + 15, m[s10[8]],  m[s10[9]]);
+        mix(v + 1, v + 6, v + 11, v + 12, m[s10[10]], m[s10[11]]);
+        mix(v + 2, v + 7, v + 8,  v + 13, m[s10[12]], m[s10[13]]);
+        mix(v + 3, v + 4, v + 9,  v + 14, m[s10[14]], m[s10[15]]);
+
+const uint8_t   *s11 = blake2b_sigma[11];
+        mix(v + 0, v + 4, v + 8,  v + 12, m[s11[0]],  m[s11[1]]);
+        mix(v + 1, v + 5, v + 9,  v + 13, m[s11[2]],  m[s11[3]]);
+        mix(v + 2, v + 6, v + 10, v + 14, m[s11[4]],  m[s11[5]]);
+        mix(v + 3, v + 7, v + 11, v + 15, m[s11[6]],  m[s11[7]]);
+        mix(v + 0, v + 5, v + 10, v + 15, m[s11[8]],  m[s11[9]]);
+        mix(v + 1, v + 6, v + 11, v + 12, m[s11[10]], m[s11[11]]);
+        mix(v + 2, v + 7, v + 8,  v + 13, m[s11[12]], m[s11[13]]);
+        mix(v + 3, v + 4, v + 9,  v + 14, m[s11[14]], m[s11[15]]);
+
         st->h[0] ^= v[0] ^ v[8];
 	st->h[1] ^= v[1] ^ v[9];
 	st->h[2] ^= v[2] ^ v[10];
